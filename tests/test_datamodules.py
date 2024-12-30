7496dfafeeb20b007c36d0b5c34464df2ab0573f
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2024-12-30 11:13:06 +0100

    Fix MNIST datamodule test to use a temporary path

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -7,23 +7,24 @@ from lightning_hydra_template.data.mnist_datamodule import MNISTDataModule
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
-def test_mnist_datamodule(batch_size: int) -> None:
+def test_mnist_datamodule(tmp_path: Path, batch_size: int) -> None:
     """Tests that `MNISTDataModule` is working as expected.
 
     It verifies that the data is downloaded correctly, that the necessary attributes were created (e.g., the dataloader
     objects), and that dtypes and batch sizes correctly match.
 
     Args:
+        tmp_path: The temporary data path.
         batch_size: Batch size of the data to be loaded by the dataloader.
     """
-    data_dir = "data/"
+    data_dir = tmp_path / "data"
 
-    dm = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
+    dm = MNISTDataModule(data_dir=str(data_dir), batch_size=batch_size)
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_val and not dm.data_test
-    assert Path(data_dir, "MNIST").exists()
-    assert Path(data_dir, "MNIST", "raw").exists()
+    assert (data_dir / "MNIST").exists()
+    assert (data_dir / "MNIST" / "raw").exists()
 
     dm.setup()
     assert dm.data_train and dm.data_val and dm.data_test
