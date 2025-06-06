87948548b46204c9230835e0506e62d2edfdfac7
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-06-06 14:01:09 +0200

    Move custom OmegaConf resolvers to the Hydra config package

--- src/lightning_hydra_template/utils/utils.py
+++ src/lightning_hydra_template/utils/utils.py
@@ -1,8 +1,6 @@
 import copy
-import importlib
 import itertools
 import math
-import operator
 import warnings
 from collections.abc import Callable
 from functools import wraps
@@ -15,41 +13,10 @@ from hydra.core.override_parser.overrides_parser import OverridesParser
 from hydra.utils import call
 from omegaconf import DictConfig, OmegaConf
 
+from lightning_hydra_template.configs import register_omegaconf_resolvers
 from lightning_hydra_template.utils import pylogger, rich_utils
 
 log = pylogger.RankedLogger(__name__, rank_zero_only=True)
-
-
-def import_from_module(dotpath: str) -> Any:
-    """Dynamically imports an object from a module based on its "dotpath".
-
-    Args:
-        dotpath: "Dotpath" (i.e. name that can be looked up via importlib) where the firsts components specify the
-            module to look up, and the last component is the attribute to import from this module.
-
-    Returns:
-        Target object.
-    """
-    module, module_attr = dotpath.rsplit(".", 1)
-    return getattr(importlib.import_module(module), module_attr)
-
-
-def register_omegaconf_resolvers() -> None:
-    """Registers custom OmegaConf resolvers."""
-
-    def _assert(condition: bool, throw_on_fail: bool = True) -> bool:
-        """Assert if condition is `True`, either raising an exception or logging a warning."""
-        if not condition:
-            if throw_on_fail:
-                raise AssertionError("Assertion of Hydra configuration failed!")
-            log.warning("Assertion of Hydra configuration failed!")
-        return condition
-
-    OmegaConf.register_new_resolver(
-        "assert", lambda condition, throw_on_fail=True: _assert(condition, throw_on_fail=throw_on_fail)
-    )
-    OmegaConf.register_new_resolver("op", lambda op, *args: getattr(operator, op)(*args))
-    OmegaConf.register_new_resolver("call", lambda fn_path, *args: import_from_module(fn_path)(*args))
 
 
 def pre_hydra_routine() -> None:
